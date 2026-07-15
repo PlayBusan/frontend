dda0d8770a6bfecb0ac1946a012f23246e325c3f
SuyeonShinnnn <suyeons5536@gmail.com>
2026-07-15 16:13:16 +0900

    feat: 마커 호버 시 축제 정보 뜨도록 변경

--- src/pages/tour/TourPage.vue
+++ src/pages/tour/TourPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <KakaoMap />
+  <div class="relative h-screen">
+    <KakaoMap @selectFestival="selectedFestival = $event" />
+
+    <FestivalCard v-if="selectedFestival" :festival="selectedFestival" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import KakaoMap from './components/KakaoMap.vue'
+import FestivalCard from '@/pages/home/components/FestivalCard.vue'
+
+const selectedFestival = ref(null)
 </script>
--- src/pages/tour/components/KakaoMap.vue
+++ src/pages/tour/components/KakaoMap.vue
@@ -1,5 +1,30 @@
 <template>
-  <div ref="mapContainer" class="relative w-full h-screen rounded-xl"></div>
+  <div class="relative w-full h-screen">
+    <div ref="mapContainer" class="w-full h-full rounded-xl"></div>
+
+    <!-- 마커 클릭시 카드 -->
+    <div v-if="selectedFestival" class="absolute bottom-8 left-1/2 -translate-x-1/2 z-50">
+      <div class="w-72 overflow-hidden rounded-3xl bg-white shadow-xl border border-slate-100">
+        <div class="relative h-44 w-full bg-slate-100">
+          <img :src="selectedFestival.image" class="h-full w-full object-cover" />
+        </div>
+
+        <div class="p-5">
+          <div class="mb-3 inline-flex rounded-full bg-secondary/20 px-3 py-1">
+            <span class="text-xs font-bold text-primary"> #축제 </span>
+          </div>
+
+          <h4 class="text-lg font-black text-slate-800">
+            {{ selectedFestival.title }}
+          </h4>
+
+          <p class="text-sm text-gray-600">
+            {{ selectedFestival.address }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,6 +34,11 @@ import { loadKakaoMap } from '@/utils/loadKakaoMap'
 
 const route = useRoute()
 const mapContainer = ref<HTMLDivElement | null>(null)
+const selectedFestival = ref<any>(null)
+
+const title = route.query.title as string
+const address = route.query.address as string
+const image = route.query.image as string
 
 onMounted(async () => {
   await loadKakaoMap()
@@ -25,21 +55,34 @@ onMounted(async () => {
   if (!address) return
 
   const geocoder = new window.kakao.maps.services.Geocoder()
-  console.log(route.query.address)
+
   geocoder.addressSearch(address, (result: any, status: any) => {
-    console.log(address)
-    console.log(status)
-    console.log(result)
     if (status !== window.kakao.maps.services.Status.OK) return
 
     const coords = new window.kakao.maps.LatLng(Number(result[0].y), Number(result[0].x))
 
-    new window.kakao.maps.Marker({
+    const marker = new window.kakao.maps.Marker({
       map,
       position: coords,
     })
 
     map.setCenter(coords)
+
+    console.log(image, title, address)
+
+    // 마우스를 올리면 카드 표시
+    window.kakao.maps.event.addListener(marker, 'mouseover', () => {
+      selectedFestival.value = {
+        title,
+        address,
+        image,
+      }
+    })
+
+    // 마우스를 떼면 카드 숨김
+    window.kakao.maps.event.addListener(marker, 'mouseout', () => {
+      selectedFestival.value = null
+    })
   })
 })
 </script>
